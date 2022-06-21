ed270a453f424a6a1dd9e1e6af2b0f1cf7c274c8
suresh <suresh@192.168.1.4>
2022-06-21 11:35:55 +0530

    First Commit

---
+++ SpringBeanLifeCycle/src/main/java/com/springbean/Test.java
@@ -0,0 +1,18 @@
+package com.springbean;
+
+import java.sql.SQLException;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		@SuppressWarnings("resource")
+		ApplicationContext a=new ClassPathXmlApplicationContext("beans.xml");
+		StudentDao d=a.getBean("studentdao",StudentDao.class);
+		d.selectAllRows();
+
+	}
+
+}
