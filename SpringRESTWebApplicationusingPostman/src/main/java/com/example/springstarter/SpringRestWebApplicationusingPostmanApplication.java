859ce86e0f80f962441acce665f56467ea4af252
suresh <suresh@192.168.1.4>
2022-06-21 11:39:02 +0530

    First Commit

---
+++ SpringRESTWebApplicationusingPostman/src/main/java/com/example/springstarter/SpringRestWebApplicationusingPostmanApplication.java
@@ -0,0 +1,13 @@
+package com.example.springstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestWebApplicationusingPostmanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestWebApplicationusingPostmanApplication.class, args);
+	}
+
+}
---
+++ SpringRESTWebApplicationusingPostman/src/test/java/com/example/SpringRESTWebApplication/SpringRestWebApplicationusingPostmanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringRESTWebApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestWebApplicationusingPostmanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
